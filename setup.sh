a9c85f3344b2eadf90eb0cf2443b582c298b8b4c
21f1003978 <109907150+21f1003978@users.noreply.github.com>
2022-07-24 19:11:26 +0530

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,11 @@
+mkdir -p ~/.streamlit/
+echo "\
+[general]\n\
+email = \"21f1003978@student.onlinedegree.iitm.ac.in\"\n\
+" > ~/.streamlit/credentials.toml
+echo "\
+[server]\n\
+headless = true\n\
+enableCORS=false\n\
+port = $PORT\n\
+" > ~/.streamlit/config.toml
