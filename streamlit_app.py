c25ce4c3c08b5bbc76104ec7dba24aee80ad1853
21f1003978 <109907150+21f1003978@users.noreply.github.com>
2022-07-24 18:56:02 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,3 +4,8 @@ import pandas as pd
 st.write(""" 
 #My first app
 """)
+
+sysnum1 = st.number_input(label=“Provide first number”,step=1.,format="%.2f")
+sysnum2 = st.number_input(label=“Provied second number ”,step=1.,format="%.2f")
+
+
