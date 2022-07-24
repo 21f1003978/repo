49737be1ab361067237786d4b1b0212b402e6c16
21f1003978 <109907150+21f1003978@users.noreply.github.com>
2022-07-24 19:01:24 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,4 +8,6 @@ st.write("""
 num_1 = st.number_input(label="Enter number#1",step=1.,format="%.2f")
 num_2 = st.number_input(label="Enter number#2",step=1.,format="%.2f")
 
-st.text('This is some text.')
+num_3 = num_1 + num_2
+st.metric(label="Addition is", value=num_3,delta_color="inverse")
+
